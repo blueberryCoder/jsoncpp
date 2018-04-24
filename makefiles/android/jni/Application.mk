bc9e4dcca16e1958736b41b6a79b8a4f3d0e098d
穆永刚 <ymu2@xiaohongshu.com>
2018-04-24 13:13:37 +0800

    add android/jni for ndk-build

---
+++ makefiles/android/jni/Application.mk
@@ -0,0 +1,11 @@
+#--------------------------------------
+# Author:blueberry
+# ------------------------------------
+
+
+NDK_TOOLCHAIN_VERSION:=4.9
+APP_ABI :=all
+APP_PLATFORM:=android-16
+APP_STL :=gunstl_static
+
+
